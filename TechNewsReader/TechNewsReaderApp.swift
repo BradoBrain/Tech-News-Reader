74b73b4f21f721debae28a1c510efb4bc021c9b9
Brado Brain <vinogradov.artem@outlook.com>
2022-11-29 11:00:11 +0100

    Initial Commit

---
+++ TechNewsReader/TechNewsReaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  TechNewsReaderApp.swift
+//  TechNewsReader
+//
+//  Created by Artem Vinogradov on 29.11.2022.
+//
+
+import SwiftUI
+
+@main
+struct TechNewsReaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
